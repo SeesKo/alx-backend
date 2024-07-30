686db590df5c66c0db842ed204bc68373e643f42
SeesKo <seeskome@seeskosee.com>
2024-07-30 04:25:22 -0700

    parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Flask application with Flask-Babel for internationalization
+and locale selection.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Configuration class for Flask application.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best language for the user from the Accept-Language header.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the index page with a welcome message.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
