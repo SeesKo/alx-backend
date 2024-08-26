10fba87cc918a3d76e83ca3eab567d85d027a585
SeesKo <seeskome@seeskosee.com>
2024-08-26 12:13:10 +0000

    process jobs from queue

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,34 @@
+import kue from 'kue';
+
+// Blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+// Function to send notifications
+function sendNotification(phoneNumber, message, job, done) {
+  // Track job progress
+  job.progress(0, 100);
+
+  // Check if the phone number is blacklisted
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    // Fail the job with an error if the number is blacklisted
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+
+  // Track job progress to 50%
+  job.progress(50, 100);
+
+  // Log the sending of the notification
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  // Complete the job
+  done();
+}
+
+const queue = kue.createQueue();
+
+// Process jobs from the 'push_notification_code_2' queue
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
